6d67e5c76f134ea475866f17878ff4de91922ef2
sample-text-here <yesthisisbot@gmail.com>
2021-01-11 18:19:53 -0800

    file saving/loading

--- src/libs/files.ts
+++ src/libs/files.ts
@@ -3,8 +3,10 @@ import * as fs from "fs";
 import { remote } from "electron";
 const dataDir = remote.app.getPath("userData");
 const sketchPath = join(dataDir, "sketch.js");
+const filesDir = join(dataDir, "files");
 
 if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir, { recursive: true });
+if (!fs.existsSync(filesDir)) fs.mkdirSync(filesDir, { recursive: true });
 if (!fs.existsSync(sketchPath)) fs.writeFileSync(sketchPath, "");
 
 export function saveSketch(value) {
@@ -13,4 +15,29 @@ export function saveSketch(value) {
 
 export function loadSketch() {
   return fs.readFileSync(sketchPath, "utf8");
+}
+
+export function saveFile(path, value) {
+  fs.writeFileSync(path, value);
+}
+
+export function openFile(path) {
+  return fs.readFileSync(path, "utf8");
+}
+
+export function fileOpen() {
+  return remote.dialog.showOpenDialogSync({
+    title: "open file",
+    properties: ["openFile"],
+    filters: [{ name: "js", extensions: ["js"] }],
+    defaultPath: filesDir,
+  });
+}
+
+export function fileSave() {
+  return remote.dialog.showSaveDialogSync({
+    title: "save file",
+    filters: [{ name: "js", extensions: ["js"] }],
+    defaultPath: filesDir,
+  });
 }
--- src/window/main.ts
+++ src/window/main.ts
@@ -4,7 +4,8 @@ import { Console } from "../ui/console";
 import { Bar } from "../ui/dragBar";
 import { formatWithCursor as format } from "prettier";
 import { run } from "../libs/run";
-import { saveSketch, loadSketch } from "../libs/files";
+import * as files from "../libs/files";
+import { basename } from "path";
 
 const main = document.getElementById("main");
 const [edit, bar, consol] = [
@@ -15,16 +16,41 @@ const [edit, bar, consol] = [
 
 bar.element.style.gridArea = "resize";
 
-edit.editor.session.on("change", () =>
-  saveSketch(edit.editor.session.getValue())
-);
+let filePath = null,
+  updated = false;
+
+edit.editor.session.on("change", () => {
+  files.saveSketch(edit.editor.session.getValue());
+  updated = true;
+  updateTitle();
+});
 
 edit.listen("save", "ctrl-s", (editor) => {
-  console.log("saving", editor.session.getValue());
+  if (!filePath) filePath = files.fileSave();
+  if (!filePath) return;
+  updated = false;
+  updateTitle();
+  files.saveFile(filePath, editor.session.getValue());
+});
+
+edit.listen("saveAs", "ctrl-shift-s", (editor) => {
+  filePath = files.fileSave();
+  if (!filePath) return;
+  updated = false;
+  updateTitle();
+  files.saveFile(filePath, editor.session.getValue());
 });
 
 edit.listen("open", "ctrl-o", () => {
-  console.log("open");
+  if (updated && filePath && !window.confirm("your file isnt saved, continue?"))
+    return;
+  filePath = files.fileOpen()[0];
+  if (!filePath) return;
+  queueMicrotask(() => {
+    updated = false;
+    updateTitle();
+  });
+  edit.editor.session.setValue(files.openFile(filePath));
 });
 
 edit.listen("format", "ctrl-alt-s", (editor) => {
@@ -32,7 +58,7 @@ edit.listen("format", "ctrl-alt-s", (editor) => {
   const index = editor.session.doc.positionToIndex(cursor);
   const value = editor.session.getValue();
   const result = format(value, { cursorOffset: index, parser: "babel" });
-  editor.session.doc.setValue(result.formatted, -1);
+  editor.session.doc.setValue(result.formatted);
   const position = editor.session.doc.indexToPosition(result.cursorOffset);
   editor.moveCursorToPosition(position);
 });
@@ -55,4 +81,12 @@ ipcRenderer.on("runCode", (e) => {
   consol.log(res);
 });
 
-edit.editor.session.doc.setValue(loadSketch(), -1);
+function updateTitle() {
+  if (filePath) {
+    document.title = "jside - " + basename(filePath) + (updated ? "*" : "");
+  } else {
+    document.title = "jside";
+  }
+}
+
+edit.editor.session.doc.setValue(files.loadSketch());
